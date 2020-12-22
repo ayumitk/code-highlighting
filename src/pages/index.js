1ab5752efbadaca501d4eeabe97189c368ef64c6
ayumitk <ayumee528@gmail.com>
2020-12-22 14:07:04 -0800

    add: PrismJSを追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,41 @@
-import React from "react"
+// ↓ useEffectを追加
+import React, { useEffect } from "react"
 import { Link } from "gatsby"
+
+// ↓ PrismJSを追加
+import Prism from "prismjs"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+// ↓ このコードが表示されます
+const code = `class HelloMessage extends React.Component {
+  render() {
+    return(
+      <div>Hello!</div>
+    )
+  }
+}
+`
+
+const IndexPage = () => {
+
+  {/* ↓ highlightAll()ファンクションをコール */}
+  useEffect(() => {
+    Prism.highlightAll()
+  })
+
+  return(
   <Layout>
     <SEO title="Home" />
+    <h3>
+      Gatsbyで作ったWebサイト内のコードブロックを、<code>PrismJS</code>を使って、シンタックスをハイライト表示する
+    </h3>
+    {/* ↓ コードブロックを追加 */}
+    <pre>
+      <code>{code}</code>
+    </pre>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -18,5 +46,6 @@ const IndexPage = () => (
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
+  }
 
 export default IndexPage
